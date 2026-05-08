5a0014bb94c6a5523de6c5bfa3f85a427f7ed4f6
Daniele-SS <dev.daniele@outlook.com>
2026-05-08 11:06:00 -0300

    feat: adiciona a tabela de advogado junto a sua chave estrangeira

--- scriptSQL.sql
+++ scriptSQL.sql
@@ -20,7 +20,7 @@ desc tbl_estado;
 desc tbl_cidade;
 desc tbl_endereco;
 desc tbl_cliente;
-
+desc tbl_advogado;
 
 create table tbl_estado (
 	id 		int not null primary key auto_increment,
@@ -59,12 +59,12 @@ create table tbl_endereco (
 
 
 create table tbl_cliente (
-	id int not null primary key auto_increment,
-    nome varchar(150),
-    cpf varchar (25) not null,
-    data_nascimento date not null,
-    sexo varchar(3) not null,
-    email varchar(255),
+	id 					int not null primary key auto_increment,
+    nome 				varchar(150),
+    cpf 				varchar (25) not null,
+    data_nascimento 	date not null,
+    sexo 				varchar(3) not null,
+    email 				varchar(255),
     
     id_endereco int not null,
     
@@ -79,7 +79,23 @@ create table tbl_cliente (
 );
 
 
-
+create table tbl_advogado (
+	id 				int not null primary key auto_increment,
+    nome 			varchar(100) not null,
+    cpf 			varchar(30) not null,
+    especialidade 	varchar(100) not null,
+    admissao 		date not null,
+    data_nascimento date not null,
+    oab 			int not null,
+    salario 		decimal(10.2) not null,
+    email 			varchar(255) not null,
+    
+    id_endereco int not null,
+    
+    constraint FK_ADVOGADO_ENDERECO
+    foreign key(id_endereco)
+    references tbl_endereco(id)
+);
 
 
 
